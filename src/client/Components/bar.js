81451985e7c7ce149e5ec637a2bee26d45d996c4
Cris <Crisdvx@outlook.com>
2020-10-05 22:26:03 -0500

    Router

--- src/client/Components/bar.js
+++ src/client/Components/bar.js
@@ -8,13 +8,13 @@ export default class bar extends Component {
   render() {
     return (
       <div style={{ color: "white" }}>
-        <nav className="navbar nav-expand-lg sticky-top">
+        <nav className="navbar nav-expand-lg sticky-top justify-content-center">
           <div id="navLogo" >
             <img src={Logo} alt="IMAGEN NO DISPONIBLE" style={{width:"240px", height:"42px"}} />
           </div>
 
           <div className="">
-            <a href="https://www.instagram.com/neuron_remoter/"><span  style={{marginRight:"30px"}} >Siguenos</span></a>
+            <a href="https://www.instagram.com/neuron_remoter/" style={{textDecoration:"none"}} ><span  style={{marginRight:"30px", textDecoration:"none", color:"white"}} >Siguenos</span></a>
             <a href="https://www.instagram.com/neuron_remoter/"><img src={IG} alt="IG"  style={{width:"30px", height:"30px", marginRight:"140px"}} /></a>
         </div>
         </nav>
