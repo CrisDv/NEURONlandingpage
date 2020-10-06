aa46cf26de4ce8dbf10f0a8a44a7765814a01b2f
Cris <Crisdvx@outlook.com>
2020-10-06 18:58:51 -0500

    ready x2

--- src/client/Components/bar.js
+++ src/client/Components/bar.js
@@ -13,7 +13,7 @@ export default class bar extends Component {
             <img src={Logo} alt="IMAGEN NO DISPONIBLE" style={{width:"240px", height:"42px"}} />
           </div>
 
-          <div className="Sig">
+          <div>
             <a href="https://www.instagram.com/neuron_remoter/" style={{textDecoration:"none"}}><span  style={{marginRight:"30px", textDecoration:"none", color:"white"}} >Síguenos</span></a>
             <a href="https://www.instagram.com/neuron_remoter/" ><img src={IG} alt="IG"  style={{width:"30px", height:"30px", marginRight:"140px"}} /></a>
         </div>
